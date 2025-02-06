9db53b9a8aa56dd6cbf05bd2c382b3d879941272
Nguyen Van Hoang <Hoangculus2.5@gmail.com>
2025-02-07 00:21:22 +0700

    Hoang 7/2

--- js.js
+++ js.js
@@ -1,3 +1,4 @@
+// xử lý cuộn tới room
 const roomContainer = document.querySelector('.room_container');
 const roomItems = document.querySelectorAll('.room_item');
 
@@ -10,4 +11,26 @@ window.addEventListener('scroll', function() {
             roomItem.classList.remove('visible'); // Xóa lớp visible khi không còn trong viewport
         }
     });
+});
+
+
+// xử lý đóng sidebar
+const clickHide = document.querySelectorAll('.x, .logo, .clickhide, .sidebar_item a, .sidebar_overlay')
+const sideBar = document.querySelector('.sidebar');
+const sideBarContainer = document.querySelector('.sidebar_container');
+const overlay = document.querySelector('.sidebar_overlay');
+clickHide.forEach(el => {
+    el.addEventListener('click', () => {
+        sideBar.classList.add("hide");
+        sideBarContainer.classList.add("hide");
+        overlay.classList.add("hide");
+    });
+});
+
+// mở sidebar
+const navBar = document.querySelector('.navbar_icon');
+navBar.addEventListener('click', () => {
+    sideBar.classList.remove("hide");
+    sideBarContainer.classList.remove("hide");
+    overlay.classList.remove("hide");
 });
